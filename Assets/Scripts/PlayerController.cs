c9436fbfa10681256b2fbc9eac1fc7c2fc308d41
Justin Ho <justinhodev@gmail.com>
2020-07-13 14:52:21 -0700

    Week 3 - Space Robot Kyle Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float walkSpeed = 2f;
     public float jumpPower = 2f;
+    public float groundDistance = 1f;
+    public float turnSpeed = 1f;
+    public LayerMask groundLayer;
 
+    private Animator _animator;
     private Rigidbody _rigidbody;
     private float _playerSpeed;
-    private float moveX;
-    private float moveY;
+    private float _moveX;
+    private float _moveY;
+    private bool _isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
+        _animator = GetComponent<Animator>();
         _playerSpeed = walkSpeed;
         _rigidbody = GetComponent<Rigidbody>();
     }
@@ -21,19 +28,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal");
-        moveY = Input.GetAxis("Vertical");
+        // Get our key input for movement
+        _moveX = Input.GetAxis("Horizontal");
+        _moveY = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            _rigidbody.AddForce(Vector3.up * jumpPower);
-        }
+        ChangeAnimation();
+        LookAt();
+        Jump();
     }
 
     private void FixedUpdate()
     {
-        Vector3 move = new Vector3(moveX, 0f, moveY);
+        Vector3 move = new Vector3(_moveX, 0f, _moveY);
         _rigidbody.MovePosition(_rigidbody.position + move * _playerSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && _isGrounded == true)
+        {
+            _rigidbody.AddForce(Vector3.up * jumpPower);
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.down, groundDistance, groundLayer))
+        {
+            Debug.DrawRay(transform.position, Vector3.down * groundDistance, Color.green);
+            _isGrounded = true;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, Vector3.down * groundDistance, Color.red);
+            _isGrounded = false;
+        }
+    }
+
+    private void LookAt()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 turn;
+        if (mouseX > 0)
+        {
+            turn = Vector3.right;
+        }
+        else
+        {
+            turn = Vector3.left;
+        }
+        transform.rotation *= Quaternion.Slerp(Quaternion.identity, Quaternion.LookRotation(turn), Math.Abs(mouseX) * turnSpeed * Time.deltaTime);
+    }
+
+    private void ChangeAnimation()
+    {
+        _animator.SetFloat("Right", _moveX);
+        _animator.SetFloat("Forward", _moveY);
     }
 
 }
